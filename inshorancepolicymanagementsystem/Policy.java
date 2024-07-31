ba0d73d3f8bedd3c193603b9945cffbee7bf2b02
suraj <suraj.jotiram@godigin.com>
2024-08-01 01:42:03 +0530

    Policy class is added

---
+++ inshorancepolicymanagementsystem/Policy.java
@@ -0,0 +1,76 @@
+package inshorancepolicymanagementsystem;
+
+import java.time.LocalDate;
+
+class Policy implements Comparable<Policy> {
+    private String policyNumber;
+    private String policyholderName;
+    private LocalDate expiryDate;
+    private String coverageType;
+    private double premiumAmount;
+
+    public Policy(String policyNumber, String policyholderName, LocalDate expiryDate, String coverageType, double premiumAmount) {
+        this.policyNumber = policyNumber;
+        this.policyholderName = policyholderName;
+        this.expiryDate = expiryDate;
+        this.coverageType = coverageType;
+        this.premiumAmount = premiumAmount;
+    }
+
+    public String getCoverageType() {
+        return coverageType;
+    }
+
+    public void setCoverageType(String coverageType) {
+        this.coverageType = coverageType;
+    }
+
+    public LocalDate getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(LocalDate expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public String getPolicyholderName() {
+        return policyholderName;
+    }
+
+    public void setPolicyholderName(String policyholderName) {
+        this.policyholderName = policyholderName;
+    }
+
+    public String getPolicyNumber() {
+        return policyNumber;
+    }
+
+    public void setPolicyNumber(String policyNumber) {
+        this.policyNumber = policyNumber;
+    }
+
+    public double getPremiumAmount() {
+        return premiumAmount;
+    }
+
+    public void setPremiumAmount(double premiumAmount) {
+        this.premiumAmount = premiumAmount;
+    }
+
+    @Override
+    public String toString() {
+        return "Policy{" +
+                "coverageType='" + coverageType + '\'' +
+                ", policyNumber='" + policyNumber + '\'' +
+                ", policyholderName='" + policyholderName + '\'' +
+                ", expiryDate=" + expiryDate +
+                ", premiumAmount=" + premiumAmount +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Policy other) {
+        return this.expiryDate.compareTo(other.expiryDate);
+    }
+
+}
