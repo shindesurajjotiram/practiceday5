3570d3c3df912b6e6c7393c99bc3b3f615ad7912
suraj <suraj.jotiram@godigin.com>
2024-08-01 01:39:16 +0530

    first non dublicate element finding program is added

---
+++ firstnondublicateelement/FirstNonDuplicateElement.java
@@ -0,0 +1,23 @@
+package firstnondublicateelement;
+
+
+import java.util.LinkedHashSet;
+import java.util.List;
+
+public class FirstNonDuplicateElement {
+
+    public static void main(String[] args) {
+        List<Integer> inputList = List.of(4, 5, 4, 6, 4, 7, 8, 5);
+        LinkedHashSet<Integer> seen = new LinkedHashSet<>();
+
+        for (int num : inputList) {
+            if (!seen.contains(num)) {
+                seen.add(num);
+            } else {
+                seen.remove(num);
+            }
+        }
+
+        System.out.println("First non-duplicate element: " + seen.iterator().next());
+    }
+}
