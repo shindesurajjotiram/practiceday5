33005aede5e23631ef97651599f9b301c9098cd2
suraj <suraj.jotiram@godigin.com>
2024-08-01 01:43:17 +0530

    main class isadded

---
+++ inshorancepolicymanagementsystem/Main.java
@@ -0,0 +1,18 @@
+package inshorancepolicymanagementsystem;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        PolicyManager policyManager = new PolicyManager();
+
+        Policy policy1 = new Policy("123","suraj shine", LocalDate.of(2017, 1, 13),"helth",200000);
+        Policy policy2 = new Policy("123","suraj",LocalDate.of(2019, 1, 13),"helth",200000);
+
+        policyManager.addPolicy(policy1);
+        policyManager.addPolicy(policy2);
+
+
+    }
+}
+
